4b77c4a5db3be57a5e0df8ce4e9b615e5ebe3762
Jorge Alvarado <jalvaradoas39@outlook.com>
2019-01-15 14:07:08 -0500

    setup package.json, server with express, static files and heroku port

---
+++ server.js
@@ -0,0 +1,46 @@
+const express = require('express');
+
+
+const port = process.env.PORT || 3000;
+console.log(port);
+
+// create new express app
+var app = express();
+
+
+// middleware
+app.use(express.static(__dirname + '/public'));
+
+
+// respond to request
+app.get('/', (req, res) => {
+	//res.send('<h1>Hello World!</h1>');
+	res.send({
+		name: 'Jorge Alvarado',
+		likes: [
+			'Working out',
+			'Yoga',
+			'Dancing',
+			'Programming',
+			'Traveling',
+			'Animals'
+		]
+	})
+});
+
+
+app.get('/about', (req, res) => {
+	res.send('<h1>About Me</h1>');
+});
+
+
+app.get('/bad', (req,res) => {
+	res.send({
+		errorMessage: 'You have entered invalid data'
+	})
+});
+
+// bind application to port on machine
+app.listen(port, () => console.log(`We are listening on port ${port}`));
+
+
