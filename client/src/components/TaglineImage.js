a34a83aafdb5eb7b476fd6712ab2505cb3cee517
Jorge Alvarado <jalvaradoas39@outlook.com>
2019-02-07 09:55:07 -0500

    Change path for tagline image

--- client/src/components/TaglineImage.js
+++ client/src/components/TaglineImage.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
+import tagline from '../tagline.jpg';
 import '../App.css';
 import TaglineDescription from './TaglineDescription';
 
 class TaglineImage extends Component {
 	render() {
-		const imgPath = process.env.PUBLIC_URL + '/images/tagline.jpg';
 		return (
 			<div className="tagline-image">
-				<img src={imgPath} className="img-fluid float-right" alt="Jorge Alvarado" />
+				<img src={tagline} className="img-fluid float-right" alt="Jorge Alvarado" />
 				<TaglineDescription />
 			</div>
 		);
