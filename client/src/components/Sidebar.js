6a87f968ed74c0c634df17278ea973c8be510edb
Jorge Alvarado <jalvaradoas39@outlook.com>
2019-02-08 22:06:02 -0500

    Add static UI to SidebarList component

--- client/src/components/Sidebar.js
+++ client/src/components/Sidebar.js
@@ -4,9 +4,14 @@ import '../App.css';
 class Sidebar extends Component {
 	render() {
 		return (
-			<div className="sidebar">
-				<h4>Inside Sidebar</h4>
-			</div>
+			<>
+				<li className="list list-group-item">
+					<a href="#" className="float-left">
+						<this.props.iconName />
+						<span className="link-name">{this.props.linkName}</span>
+					</a>
+				</li>
+			</>
 		);
 	}
 }
--- client/src/components/SidebarList.js
+++ client/src/components/SidebarList.js
@@ -2,13 +2,40 @@ import React, { Component } from 'react';
 import '../App.css';
 import Sidebar from './Sidebar';
 
+/* REACT ICONS */
+import { FaUserSecret } from 'react-icons/fa';
+import { GoTools } from 'react-icons/go';
+import { FaClipboardList } from 'react-icons/fa';
+import { IoMdTrendingUp } from 'react-icons/io';
+import { IoMdSchool } from 'react-icons/io';
+import { IoIosMail } from 'react-icons/io';
+
+
+
 class SidebarList extends Component {
 	render() {
 		return (
 			<div className="sidebar-list">
-				<h3>Inside SidebarList</h3>
-				<Sidebar />
-				<Sidebar />
+				<ul className="list-group">
+					<Sidebar
+						iconName={FaUserSecret}
+						linkName="About" />
+					<Sidebar
+						iconName={GoTools}
+						linkName="Skills" />
+					<Sidebar
+						iconName={FaClipboardList}
+						linkName="Projects" />
+					<Sidebar
+						iconName={IoMdTrendingUp}
+						linkName="Experience" />
+					<Sidebar
+						iconName={IoMdSchool}
+						linkName="Education" />
+					<Sidebar
+						iconName={IoIosMail}
+						linkName="Contact" />
+				</ul>
 			</div>
 		);
 	}
