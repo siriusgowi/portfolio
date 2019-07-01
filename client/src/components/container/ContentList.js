acb78ceb3a1e46940373799a592fca04d399cc74
Jorge Alvarado <jalvaradoas39@outlook.com>
2019-07-01 14:21:51 -0400

    Fixing bug for image upload to heroku

--- client/src/components/container/ContentList.js
+++ client/src/components/container/ContentList.js
@@ -2,20 +2,19 @@ import React from 'react';
 
 // Components
 import TaglineImage from './content/TaglineImage';
-import About from './content/About';
-import Skills from './content/Skills';
+// import About from './content/About';
+// import Skills from './content/Skills';
 
 
 
 const ContentList = (props) => {
 	const { content } = props.contentList;
-	const { tagline, about, skills } = content;
+	const { tagline } = content;
 
 	return (
 		<div className="content-container">
 			<TaglineImage tagline={tagline} />
-			<About about={about} />
-			<Skills skills={skills} />
+
 		</div>
 	);
 	
