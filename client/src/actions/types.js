64b35da4a306b07291241938bd3f2e6e611b2692
Jorge Alvarado <jalvaradoas39@outlook.com>
2019-07-03 21:28:45 -0400

    Remove redux setup - store, actions & reducers

--- client/src/actions/types.js
+++
@@ -1,5 +0,0 @@
-export const DATA_LOADED = 'DATA_LOADED';
-export const LOADING_ERROR = 'LOADING_ERROR';
-
-
-
--- client/src/reducers/index.js
+++
@@ -1,7 +0,0 @@
-import { combineReducers } from 'redux';
-import loadReducer from './loadReducer';
-
-
-export default combineReducers({
-	load: loadReducer
-});
